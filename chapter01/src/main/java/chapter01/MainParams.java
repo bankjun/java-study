11b293afe29fe8fe6b5722eb18b7787bff8fad61
BIT <BIT@DESKTOP-7STG043>
2023-06-26 10:45:05 +0900

    기본 프로그래밍 final

---
+++ chapter01/src/main/java/chapter01/MainParams.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+public class MainParams {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+			//이렇게 하면 파나메터가 없어서 아무것도 실행되지 않음 
+			// 그래서 Run as -> Run configuration -> Arguments에 값을 넣어주면 뭐가 뜸
+			
+		}
+
+	}
+
+}
