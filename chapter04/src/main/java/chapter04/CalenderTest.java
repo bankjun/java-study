3edec2b8164910b03afbf075d443a1330b6d4953
BIT <BIT@DESKTOP-7STG043>
2023-06-29 15:37:03 +0900

    date & Calendar

---
+++ chapter04/src/main/java/chapter04/CalenderTest.java
@@ -0,0 +1,14 @@
+package chapter04;
+
+import java.util.Calendar;
+
+public class CalenderTest {
+
+	public static void main(String[] args) {
+		// 추상메소드라서 new로 객체를 생성하지 못함
+		Calendar cal = Calendar.getInstance();
+		
+
+	}
+
+}
---
+++ chapter04/src/main/java/chapter04/DateTest.java
@@ -0,0 +1,46 @@
+package chapter04;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+// ctrl + shft + o
+public class DateTest {
+
+	public static void main(String[] args) {
+		Date now = new Date();
+		printDate01(now);
+		printDate02(now);
+	}
+	
+	public static void printDate01(Date d) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String date = sdf.format(d);
+		
+		System.out.println(date);
+	}
+	
+	public static void printDate02(Date d) {
+		//년도 (+1900) y2k의 흔적
+		int year = d.getYear();
+		
+		//월 0~11 (+1)
+		int month = d.getMonth();
+		
+		//일
+		int date = d.getDate();
+		//시
+		int hours = d.getHours();
+		//분
+		int minutes = d.getMinutes();
+		//초
+		int seconds = d.getSeconds();
+		System.out.println(
+				(year+1900) + "-"+
+				(month+1)+"-" +
+				date + " " +
+				hours + ":" +
+				minutes + ":" +
+				seconds
+				);
+	}
+
+}
